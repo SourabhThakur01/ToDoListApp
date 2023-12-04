b89119218b4e9868348f3e01bc817f7969dd995b
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-12-03 17:19:41 -0800

    lab6

--- scr/components/ToDoForm.jsx
+++ scr/components/ToDoForm.jsx
@@ -1,11 +1,5 @@
-/**
- * My To Do List App
- *
- * @format
- */
-
-import React/*, { useState }*/ from 'react';
-import {StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -15,10 +9,36 @@ import AboutScreen from '../screens/About';
 const Stack = createStackNavigator();
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState('');
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch('/src/data/tasks.json');
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  const handleAddTask = () => {
+    if (tasks.length > 0) {
+      const randomIndex = Math.floor(Math.random() * tasks.length);
+      setSelectedTask(tasks[randomIndex].task);
+    }
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home">
+          {() => <HomeScreen onAddTask={handleAddTask} selectedTask={selectedTask} />}
+        </Stack.Screen>
         <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -26,7 +46,7 @@ function App() {
 }
 
 const styles = StyleSheet.create({
- 
+  
 });
 
 export default App;
